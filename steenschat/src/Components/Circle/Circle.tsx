b0b7f2c7ec8922bbaa29fb42cf022680cd9964bc
Lies De Praeter <liesdepr3@student.arteveldehs.be>
2025-12-11 12:37:42 +0100

    Update Circle.tsx

--- steenschat/src/Components/Circle/Circle.tsx
+++ steenschat/src/Components/Circle/Circle.tsx
@@ -15,51 +15,14 @@ interface CircleProps {
   index?: CircleIndex;
   children?: React.ReactNode;
 }
-{/*text?: CircleText;*/}
 
 const Circle: React.FC<CircleProps> = ({ size, color, text, index = 'default', children }) => {
-
-  {/*const textColorMap: Record<CircleText, CircleColor> = {
-    rozenkwarts: 'red',
-    citrien: 'orange',
-    aventurijn: 'lemon',
-    obsidiaan: 'blue',
-    amethist: 'purple',
-  };
-  // kies kleur: kleur-prop heeft altijd voorrang
-  const finalColor: CircleColor | undefined = color ?? (text ? textColorMap[text] : undefined);*/}
 
   // haal automatisch kleur op uit JSON
   const jsonColor: CircleColor | undefined = text ? stoneByName[text].color : undefined;
   // prop heeft voorrang
   const finalColor: CircleColor | undefined = color ?? jsonColor;
 
-  {/*// bepaal text color class automatisch
-  const textClass = (() => {
-    if (text) {
-      switch(text) {
-        case 'rozenkwarts':
-        case 'citrien':
-        case 'obsidiaan':
-          return 'text--reverse'; 
-        case 'aventurijn':
-        case 'amethist':
-        default:
-          return ''; 
-      }
-    } else if (color) {
-      switch(color) {
-      case 'red':
-      case 'orange':
-      case 'blue':
-        return 'text--reverse'; 
-      case 'lemon':
-      case 'purple':
-      default:
-        return ''; 
-      }
-    } return '';
-  })();*/}
   const reverseColors: CircleColor[] = ['red', 'orange', 'blue'];
   const textClass = reverseColors.includes(finalColor!)? 'text--reverse' : '';
 
